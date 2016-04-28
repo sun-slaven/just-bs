aa92f986fab8771ff3202f30f643a306ef990b4a
slaven <slaven893196569@gmail.com>
2016-04-28 23:41:18 +0800

    save

--- app/js/just/angular/controllers/lessons_controller.js
+++ app/js/just/angular/controllers/lessons_controller.js
@@ -1,13 +1,44 @@
 GlobalModules.add_controller('lessons')
 angular.module('just.controllers.lessons', [])
-    .controller('LessonsController', ['$rootScope', '$scope', 'LessonsService', 'CollegeMajorService','CommonUtil', function($rootScope, $scope, LessonsService, CollegeMajorService,CommonUtil) {
+    .controller('LessonsController', ['$rootScope', '$scope', 'LessonsService', 'CollegeMajorService', 'CommonUtil', function($rootScope, $scope, LessonsService, CollegeMajorService, CommonUtil) {
         $scope.lessons = $rootScope.all_lessons;
         $scope.colleges = $rootScope.all_colleges;
         $scope.majors = $rootScope.all_majors;
         $scope.chosen_college = null;
         $scope.chosen_major = null;
-        $scope.$watch('chosen_college', function(newValue, oldValue) {
-            if (newValue && $scope.chosen_major.college_id  == newValue.id) {
+
+
+        $scope.change_college = function(college) {
+            if (college) {
+                $scope.majors = college.major_list;
+                $scope.lessons = CommonUtil.getLessonsByCollege(college.id);
+            } else {
+                $scope.chosen_major = null;
+                $scope.majors = $rootScope.all_majors;
+                $scope.lessons = $rootScope.all_lessons;
+            }
+        }
+        $scope.change_major = function(major) {
+            if ($scope.chosen_college) {
+                $scope.majors = $scope.chosen_college.major_list;
+                return;
+            };
+            if (major) {
+                angular.forEach($rootScope.all_colleges, function(college) {
+                    if (college.id == major.college_id) {
+                        $scope.chosen_college = college;
+                        $scope.lessons = CommonUtil.getLessonsByMajor(major.id);
+                    };
+                })
+            } else {
+                $scope.majors = $rootScope.all_majors;
+                $scope.lessons = $rootScope.all_lessons;
+            }
+        }
+
+
+/*        $scope.$watch('chosen_college', function(newValue, oldValue) {
+            if (newValue && $scope.chosen_major.college_id == newValue.id) {
                 return;
             };
             if (newValue) {
@@ -33,7 +64,7 @@ angular.module('just.controllers.lessons', [])
                 $scope.lessons = $rootScope.all_lessons;
             }
         })
-
+*/
 
 
     }])
