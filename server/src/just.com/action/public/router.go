9d5d467cc297f3307ced410a3a39bf2287e7d298
wanghao <wanghao@wosai-inc.com>
2016-05-02 21:38:25 +0800

    callback -2 post

--- server/src/just.com/action/public/router.go
+++ server/src/just.com/action/public/router.go
@@ -3,6 +3,6 @@ import "github.com/gin-gonic/gin"
 
 func BuildRouter(group *gin.RouterGroup, path, port string) {
 	group.GET("/user_active", UserActiveHandle)
-	group.GET("/callback", UploadCallbackHandle)
+	group.POST("/callback", UploadCallbackHandle)
 	group.GET("/swagger", SwaggerHandle(path, port))
 }
